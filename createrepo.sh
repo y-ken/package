d837d6d1a1a238e5f0ae557f6a19f0200c51bead
y-ken <y.ken.studio@gmail.com>
2012-08-20 00:26:16 +0900

    fix centos 5.x rpomd hash problem

--- createrepo.sh
+++ createrepo.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-createrepo -v -d -s sha centos/5/i386/
-repoview -u http://y-ken.github.com/package/centos/5/i386/repoview/ centos/5/i386/
+#createrepo -v -d -s sha centos/5/i386/
+#repoview -u http://y-ken.github.com/package/centos/5/i386/repoview/ centos/5/i386/
 
-createrepo -v -d -s sha centos/5/x86_64/
-repoview -u http://y-ken.github.com/package/centos/5/x86_64/repoview/ centos/5/x86_64/
+createrepo -v -s sha centos/5/x86_64/
+#createrepo -v -d -s sha centos/5/x86_64/
+#repoview -u http://y-ken.github.com/package/centos/5/x86_64/repoview/ centos/5/x86_64/
 
 createrepo -v -d centos/6/x86_64/
 repoview -u http://y-ken.github.com/package/centos/6/x86_64/repoview/ centos/6/x86_64/
